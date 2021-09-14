9e10bdbbd9479783909adec38047693b5691f226
sridhar <sridhar@karaokegarage.com>
2021-09-14 17:38:14 +0530

    Update ReactExoplayerView.java

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -708,37 +708,39 @@ class ReactExoplayerView extends FrameLayout implements
 
     // AudioManager.OnAudioFocusChangeListener implementation
 
+    /* Sridhar: pause/resume not working. So commented this code temporarily*/
     @Override
     public void onAudioFocusChange(int focusChange) {
-        switch (focusChange) {
-            case AudioManager.AUDIOFOCUS_LOSS:
-                eventEmitter.audioFocusChanged(false);
-                pausePlayback();
-                audioManager.abandonAudioFocus(this);
-                break;
-            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
-                eventEmitter.audioFocusChanged(false);
-                break;
-            case AudioManager.AUDIOFOCUS_GAIN:
-                eventEmitter.audioFocusChanged(true);
-                break;
-            default:
-                break;
-        }
+        // switch (focusChange) {
+        //     case AudioManager.AUDIOFOCUS_LOSS:
+        //         eventEmitter.audioFocusChanged(false);
+        //         System.out.println("pausePlayback-1 ");
+        //         pausePlayback();
+        //         audioManager.abandonAudioFocus(this);
+        //         break;
+        //     case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+        //         eventEmitter.audioFocusChanged(false);
+        //         break;
+        //     case AudioManager.AUDIOFOCUS_GAIN:
+        //         eventEmitter.audioFocusChanged(true);
+        //         break;
+        //     default:
+        //         break;
+        // }
 
-        if (player != null) {
-            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
-                // Lower the volume
-                if (!muted) {
-                    player.setVolume(audioVolume * 0.8f);
-                }
-            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
-                // Raise it back to normal
-                if (!muted) {
-                    player.setVolume(audioVolume * 1);
-                }
-            }
-        }
+        // if (player != null) {
+        //     if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+        //         // Lower the volume
+        //         if (!muted) {
+        //             player.setVolume(audioVolume * 0.8f);
+        //         }
+        //     } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+        //         // Raise it back to normal
+        //         if (!muted) {
+        //             player.setVolume(audioVolume * 1);
+        //         }
+        //     }
+        // }
     }
 
     // AudioBecomingNoisyListener implementation
